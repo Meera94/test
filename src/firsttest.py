e91bcb78d6dce5e5bdbe28719859a7cdfdca2d93
manand <manand@vmware.com>
2016-08-30 09:27:50 -0700

    initial commit

---
+++ src/firsttest.py
@@ -0,0 +1,14 @@
+'''
+Created on Aug 30, 2016
+
+@author: manand
+'''
+def add(a,b):
+    return a+b
+
+def addFixedValue(a):
+    y = 5
+    return y +a
+  
+print add(1,2)
+print addFixedValue(1) 
