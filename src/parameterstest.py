0626fb2c0a4ab316784736d75232542c20ec88c9
manand <manand@vmware.com>
2016-08-30 12:30:49 -0700

    -c

--- src/parameterstest.py
+++ src/parameterstest.py
@@ -14,10 +14,7 @@ def main(argv):
       print 'test.py -i <inputfile> -o <outputfile>'
       sys.exit(2)
    for opt, arg in opts:
-      if opt == '-h':
-         print 'test.py -i <inputfile> -o <outputfile>'
-         sys.exit()
-      elif opt in ("-i", "--ifile"):
+      if opt in ("-i", "--ifile"):
          inputfile = arg
       elif opt in ("-o", "--ofile"):
          outputfile = arg
